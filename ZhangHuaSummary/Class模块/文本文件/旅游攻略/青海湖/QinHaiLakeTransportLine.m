9567e3941d5a0a9c9ab296fd10411a61a4e14f5a
张华 <3051942353@qq.com>
2017-09-06 00:44:59 +0800

    青海湖攻略

--- ZhangHuaSummary/Class模块/文本文件/旅游攻略/青海湖/QinHaiLakeTransportLine.m
+++ ZhangHuaSummary/Class模块/文本文件/旅游攻略/青海湖/QinHaiLakeTransportLine.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self createTextField];
+    self.view.backgroundColor = [UIColor whiteColor];
     // Do any additional setup after loading the view.
 }
 -(void)createTextField{
--- ZhangHuaSummary/Class模块/文本文件/旅游攻略/青海湖/ZHQinHaiLake.m
+++ ZhangHuaSummary/Class模块/文本文件/旅游攻略/青海湖/ZHQinHaiLake.m
@@ -22,8 +22,8 @@
     // Do any additional setup after loading the view.
 }
 -(void)initSourceData{
-    self.tableArray  = [[NSMutableArray alloc]initWithObjects:@"西宁->塔尔寺", nil];
-    self.tableArray  = [[NSMutableArray alloc]initWithObjects:
+    self.tableArray  = [[NSMutableArray alloc]initWithObjects:@"西宁->塔尔寺",@"塔卡寺->倒淌河",nil];
+    self.detailArray  = [[NSMutableArray alloc]initWithObjects:
     @"西宁至塔尔寺的公交专线,火车站(5路,9路公交车停车场),沿途经过建国路,省人民医院,共和路,昆仑桥,南川东路,总寨,徐家寨,终点至湟中汽车站,时程约1小时.每日首班车7时30分发车,末班车19时30分发车.另外,西宁至塔尔寺公交专线单程约32公里,全程投币3元,成人刷卡2.40元,学生刷卡0.5元,老年卡每次刷卡冲减两次免费乘坐.西宁至塔尔寺高速快客由10辆中型高一级燃气客运车在西塔高速一级公路上营运.始发站点为新宁路客运站和湟中汽车站,每日7时至18时30分双向对发,时程约半小时,票价初定为5.30元.公交官方电话:4001009840", nil];
 }
 -(void)creatTableView{
@@ -43,10 +43,18 @@
     return self.tableArray.count;
 }
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ID" forIndexPath:indexPath];
+    static NSString *ID = @"tableViewCellIdentifier";
     
-    return cell;
-}
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld - %@",(long)indexPath.row,self.tableArray[indexPath.row]];
+    cell.textLabel.font = [UIFont systemFontOfSize:18];
+    cell.selectionStyle = UITableViewCellSelectionStyleBlue;
+    //添加cell上最后侧箭头
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    return cell;}
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     
     QinHaiLakeTransportLine *vc = [[QinHaiLakeTransportLine alloc]init];
